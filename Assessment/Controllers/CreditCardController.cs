81be843a35b16b32698e5de5aa9589780120c6a7
salisKingdoms <aryanisalis095@gmail.com>
2024-03-05 20:16:45 +0700

    add Credit card ESB assesment

---
+++ Assessment/Controllers/CreditCardController.cs
@@ -0,0 +1,45 @@
+﻿using Assessment.Config;
+using Assessment.Data.dto;
+using Assessment.Data.Repository;
+using Assessment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Assessment.Data.dto;
+
+namespace Assessment.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        private readonly CreditCardRepo _ccRepo;
+        public CreditCardController(CreditCardRepo repo)
+        {
+            _ccRepo = repo;
+        }
+        public IActionResult CreditCards()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string GetBankTypeByNo(string ccNo)
+        {
+            RespCreditCard result = new RespCreditCard();
+            try
+            {
+                var cc = _ccRepo.GetListCCByNo(ccNo).FirstOrDefault();
+                result.bank = cc.bank;
+                result.cc_type = cc.creadit_type;
+                result.message = "success";
+                result.is_ok = true;
+
+            }
+            catch (Exception ex)
+            {
+                result.is_ok = false;
+                result.message = "Edit Failed";
+
+            }
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
---
+++ Assessment/Data/Repository/CreditCardRepo.cs
@@ -0,0 +1,37 @@
+﻿using Assessment.Data.DataAccess;
+using Assessment.Data.dto;
+using Assessment.Models;
+using System.Xml.Linq;
+
+namespace Assessment.Data.Repository
+{
+    public class CreditCardRepo
+    {
+        private readonly IsqlDataAccess _dataAccess;
+        private readonly IConfiguration _configuration;
+        public CreditCardRepo(IsqlDataAccess db, IConfiguration configuration)
+        {
+            _dataAccess = db;
+            _configuration = configuration;
+        }
+        public List<mscreditcard> GetListCCByNo(string accNo)
+        {
+            var data = GetListCC(accNo).Result.ToList();
+            return data;
+        }
+        private async Task<IEnumerable<mscreditcard>> GetListCC(string accNo)
+        {
+            string query = @"select creadit_type,bank from tbl_cc";
+
+            
+            if (!string.IsNullOrEmpty(accNo))
+            {
+                query += " where number_card like @accNo";
+            }
+            var param = new Dictionary<string, object> {
+                    { "accNo", accNo+"%" }
+                    };
+            return await _dataAccess.GetData<mscreditcard, dynamic>(query, param);
+        }
+    }
+}
