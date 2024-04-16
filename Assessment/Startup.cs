de49ef5d96576362fd465ba2f0585758a0cbd707
salisKingdoms <aryanisalis095@gmail.com>
2024-04-16 10:20:14 +0700

    edit config

--- Assessment/Startup.cs
+++ Assessment/Startup.cs
@@ -22,24 +22,7 @@ namespace Assessment
             services.AddControllersWithViews();
             services.AddHttpContextAccessor();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
-            //var appSettings = new AppSettingsModel();
-            //Configuration.GetSection("ApplicationSettings").Bind(appSettings);
-            //services.Configure<AppSettingsModel>(options =>
-            //{
-            //    options.client_secret = appSettings.client_secret;
-            //    options.client_id = appSettings.client_id;
-            //    options.client_secret = appSettings.client_secret;
-            //    options.base_api = appSettings.base_api;
-            //    options.pim_base_api = appSettings.pim_base_api;
-            //    options.session_timeout = appSettings.session_timeout;
-            //    options.aws_config = appSettings.aws_config;
-            //});
-            //services.Configure<AppSettingsModel>(Configuration.GetSection("ApplicationSettings"));
-            //services.AddSession(o =>
-            //{
-            //    o.IdleTimeout = TimeSpan.FromSeconds(Configuration.GetValue<double>("ApplicationSettings:session_timeout"));
-            //});
-            //services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+           
             services.AddMvc().AddControllersAsServices();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -75,32 +58,7 @@ namespace Assessment
             app.UseStaticFiles();
 
             app.UseRouting();
-            //app.UseSession();
-            //var cookiePolicyOptions = new CookiePolicyOptions
-            //{
-            //    MinimumSameSitePolicy = SameSiteMode.Strict,
-            //};
-            //app.UseCookiePolicy(cookiePolicyOptions);
-            //added by leonagata holianti on 28 february 2023 AR2022-000735_SPRINT30_1204037939451573
-            //untuk prevent clickjacking
-            //app.Use(async (context, next) =>
-            //{
-            //    context.Response.Headers.Add("X-Frame-Options", "DENY");
-            //    await next();
-            //});
 
-
-            //added by leonagata holianti on 28 february 2023 AR2022 - 000735_SPRINT30_1204037939451574
-            //untuk prevent HTTP TRACE
-            //app.Use(async (context, next) =>
-            //{
-            //    if (context.Request.Method == "OPTIONS")
-            //    {
-            //        context.Response.StatusCode = 405;
-            //        return;
-            //    }
-            //    await next.Invoke();
-            //});
 
 
             app.UseAuthentication();
@@ -113,13 +71,6 @@ namespace Assessment
                                   name: "default",
                                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-
-
-            //app.Use(async (context, next) =>
-            //{
-            //    context.Features.Get<IHttpMaxRequestBodySizeFeature>().MaxRequestBodySize = null; // unlimited I guess
-            //    await next.Invoke();
-            //});
         }
 
 
